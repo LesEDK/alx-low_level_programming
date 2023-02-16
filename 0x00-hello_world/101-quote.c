46b9a691143d3a66d5761f0593435f8e4685288c
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-16 10:42:02 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that prints a string to the standard error
+ * Return: 1 (error)
+ */
+int main(void)
+{
+	echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		return (1);
+}
