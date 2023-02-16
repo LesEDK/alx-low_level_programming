292a305ea81c43ce5105aaeca84cf289138e0c74
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-16 05:40:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - Programme that prints a line in puts function
+ *
+ * Return : This returns 0
+ */
+int main(void)
 {
-	char name [100]
-		fgets (name,100,stdin);
-
-		puts ("\"Programming is like building a multilingual puzzle\n");
-				
-		retun(0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
