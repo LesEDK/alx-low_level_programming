4bb0134ac13549dd4daf9c1a7bef6d0a71290673
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-16 08:46:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Programme that prints a line in puts function
- *
- * Return : This returns 0
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("\"Programmming is like building a multilingual puzzle");
+	return (0);
 }
