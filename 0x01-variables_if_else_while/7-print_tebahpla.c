6cb650e54222eb7c4b30267d4899edc20d029b8e
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-18 03:28:50 -0800

    7-print_tepahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the lowercase in reverse
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'z';
+	while
+		(b <= 'a') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
