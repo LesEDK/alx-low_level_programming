410d1853cee2583cfadfcd20022c3d5f27c158d5
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-21 07:31:09 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char put = "_putchar";
+
+	while (put)
+	{
+		_putchar(put);
+		put++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
