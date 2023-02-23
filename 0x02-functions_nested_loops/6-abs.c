2b589c4028513e31ece30db96b747564f3f2c668
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-22 17:21:52 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value of an integer
+ *
+ * @a: input number as an integer
+ * Return: absolute value
+ */
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+else
+{
+return (a * -1);
+}
+}
