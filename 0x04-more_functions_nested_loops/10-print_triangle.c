01d6953fe2a81c9950ce5fac4c111245da4f687f
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-24 15:17:55 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_triangle - Function that prints triangle
+ * @size: integer that is being checked
+ * Return: void "Nothing"
+ */
+void print_triangle(int size)
+{
+int j, k;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (j = 0; j < size; j++)
+{
+for 
