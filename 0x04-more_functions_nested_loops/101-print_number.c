5715cc100c4e6238b6db0ac6508977e5d5c59a6d
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-27 07:54:40 -0800

    0-reset_to_98.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer
+ * @n: integer being checked
+ * Return: void (Nothing)
+ */
+void print_number(int n)
+{
+_putchar(n + '0');
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes pointer to an int as parameter and
+ * updatesthe value it points to 98
+ * @n: pointer being checked
+ * Return: void (Nothing).
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
