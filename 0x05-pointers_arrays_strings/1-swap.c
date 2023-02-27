3103bc0d95abe78d8477d1eab9154154e438f944
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-02-27 10:48:03 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Function that swaps the values of two intergers
+ * @a: integer being swaped
+ * @b: interger being
+ * Return: void (Nothing)
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
