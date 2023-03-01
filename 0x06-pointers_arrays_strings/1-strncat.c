fb7c8bb4549b4d3806e7c49d1d5f44d284bf87da
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-03-01 08:24:39 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,12 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int a = 0;
-int b = 0;
+int index = 0, dest_len = 0;
 
-while(dest[a] != '\0')
-{
-a++;
-}
-while(b < n && src[b] != '\0')
-{
-dest[a] = src[b];
-a++;
-b++;
-}
-dest[a] = '\0';
+while (dest[index++])
+dest_len++;
+
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
