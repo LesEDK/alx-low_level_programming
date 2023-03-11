9049e3d0081f3db4fd65bdcb65d151acba2d5929
Lesedi Komape <kinglesedikomape10@gmail.com>
2023-03-11 04:02:10 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argc: The number of arguments supplied to the program.
  * Return: 0 (Success)
  */
-int main(int __attribute__((__unused__)) arg, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
